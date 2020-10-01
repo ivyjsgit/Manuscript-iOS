2fcb5c5de839c8804c7148b2b2065574e5498319
Ivy Jackson <ivyjs@hendrix.edu>
2020-10-01 15:51:01 -0500

    Initial Commit

---
+++ Manuscript/ManuscriptApp.swift
@@ -0,0 +1,20 @@
+//
+//  ManuscriptApp.swift
+//  Manuscript
+//
+//  Created by Ivy on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ManuscriptApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
