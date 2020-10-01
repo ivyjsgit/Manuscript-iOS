3950233ee0fbaea1b63bb6ea91f3e27a2642713c
Ivy <ivyjs@hendrix.edu>
2020-10-01 18:46:14 -0500

    Switched from SwiftUI to Storyboard

---
+++ Manuscript/Manuscript/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manuscript
+//
+//  Created by Ivy on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
