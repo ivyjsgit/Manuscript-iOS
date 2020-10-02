de2882d0d22de3e601f3a4977a96e73f76619c1a
Ivy <ivyjs@hendrix.edu>
2020-10-02 08:29:09 -0500

    Ported over existing UI to SwiftUI

---
+++ Manuscript/Manuscript/SymbolEnum.swift
@@ -0,0 +1,11 @@
+//
+//  SymbolEnum.swift
+//  Manuscript
+//
+//  Created by Ivy on 10/1/20.
+//
+
+import Foundation
+enum SymbolEnum {
+    case twelveeighttime, twotwotime, twofourtime, threefourtime, threeeighttime, fourfourtime, sixeighttime, nineeighttime, barline, cclef, commontime, cuttime, dot, doublesharp, eighthnote, eighthrest, fclef, flat, gclef, halfnote, natural, quarternote, quarterrest, sharp, sixteenthnote, sixteenthrest, sixtyfournote, sixtyfourrest, thirtytwonote, thirtytworest, wholehalfrest, wholenote
+}
---
+++ Manuscript/Manuscript/SymbolPosition.swift
@@ -0,0 +1,11 @@
+//
+//  SymbolPosition.swift
+//  Manuscript
+//
+//  Created by Ivy on 10/1/20.
+//
+
+import Foundation
+enum SymbolPosition:String{
+    case a,b,c,d,e,f,g
+}
