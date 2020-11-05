5d1b3ded2e2997d0e805152e479a87946a60acca
Ivy <ivyjs@hendrix.edu>
2020-11-05 08:29:52 -0600

    Added math for finding height and width of symbols

--- Manuscript/Manuscript/Model/ImageClassifier.swift
+++ Manuscript/Manuscript/Model/ImageClassifier.swift
@@ -34,7 +34,8 @@ struct ImageClassifier{
         //???
         //Profit
         
-        let drawSize = CGSize(width: 300, height: 300)
+        //For some reason, CGImages are recognized as half of their size???
+        let drawSize = CGSize(width: 150, height: 150)
         
         let imageRenderer = UIGraphicsImageRenderer(size: drawSize)
         let symbolAsCIImage = imageRenderer.image { ctx in
@@ -46,6 +47,12 @@ struct ImageClassifier{
             
             var points = path.points
             //Do math to center points here
+            
+            let symbolWidth = abs(findMaxX(cgpoints: points)) - abs(findMinX(cgpoints: points))
+            let symbolHeight = abs(findMaxY(cgpoints: points)) - abs(findMinX(cgpoints: points))
+            
+            print("Height: \(symbolHeight) Width: \(symbolWidth)")
+
             let first = points.removeFirst()
             ctx.cgContext.move(to: first)
             ctx.cgContext.addLines(between: points)
@@ -62,5 +69,38 @@ struct ImageClassifier{
         let context = CIContext(options: nil)
         return context.createCGImage(inputImage, from: inputImage.extent)
     }
+    
+    func findMinX(cgpoints: [CGPoint]) -> Float{
+        let xs:[Float] = cgpoints.map ({(point:CGPoint) -> Float in
+            return Float(point.x
+)        })
+        return xs.min()!
+
+    }
+    
+    func findMaxX(cgpoints: [CGPoint]) -> Float{
+        let xs:[Float] = cgpoints.map ({(point:CGPoint) -> Float in
+            return Float(point.x
+)        })
+        return xs.max()!
+
+    }
+    
+    func findMinY(cgpoints: [CGPoint]) -> Float{
+        let ys:[Float] = cgpoints.map ({(point:CGPoint) -> Float in
+            return Float(point.y
+)        })
+        return ys.min()!
+
+    }
+    
+    func findMaxY(cgpoints: [CGPoint]) -> Float{
+        let ys:[Float] = cgpoints.map ({(point:CGPoint) -> Float in
+            return Float(point.y
+)        })
+        return ys.min()!
+
+    }
+    
 }
 
